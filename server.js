87e052ae47674dee1550d076e92636ab92dcb914
Ryan Bowman <rymbowman@gmail.com>
2025-02-13 13:21:44 -0500

    update CORS configuration and email sending functionality

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 const corsOptions = {
   origin: process.env.FRONTEND_URL,
   credentials: true,
+  allowedHeaders: ["Content-Type"],
 };
 app.use(cors(corsOptions));
 
